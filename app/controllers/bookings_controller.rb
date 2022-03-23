3aa8e8bc687d55ecd2dd17299f0713fd0178ec7e
Jerome Tan <jerome.tan@tuta.io>
2022-03-23 22:19:17 +0100

    finished MVC

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,6 +1,23 @@
 class BookingsController < ApplicationController
-
   def create
-    raise
+    @booking = Booking.new(booking_params)
+    @room = Room.find(params[:room_id])
+    @booking.room = @room
+    @hotel = @room.hotel
+    if @booking.save
+      redirect_to hotel_room_booking_path(@hotel, @room, @booking)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @booking = Booking.find(params[:id])
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:arrival, :departure, :guest_number)
   end
 end
